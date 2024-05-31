662d478030517860dbb9ac81ce0d43417de23c12
Nawan Ernandes <nawanernandess@gmail.com>
2024-05-30 22:39:17 -0300

    remove pasta de imagens, cria novo arquivo html e css , novo estilo do projeto.

--- inter.js
+++ inter.js
@@ -1,55 +1,19 @@
-function verificar(){
-    var data = new Date()
-    var ano  = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.querySelector('div#res')
+function verificar() {
+  var data = new Date();
+  var ano = data.getFullYear();
+  var fano = document.getElementById("txtano");
+  var res = document.querySelector("div#res");
 
-    if(fano.value.length == 0 || Number(fano.value) > ano){
-        window.alert(' [ERRO] Verifique os dados e tente novamente!')
-    }else{
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(fano.value)
-        var genero = ''
-        var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
+  if (fano.value.length == 0 || Number(fano.value) > ano) {
+    window.alert(" [ERRO] Verifique os dados e tente novamente!");
+  } else {
+    var idade = ano - Number(fano.value);
+    var genero = "";
+    var img = document.createElement("img");
+    img.setAttribute("id", "foto");
 
-        if(fsex[0].checked){
-            genero = 'Homem'
-            
-            if(idade >=0 && idade < 10){
-                //criança
-                img.setAttribute('src', 'img/bebeH.jpg')
-            }else if(idade < 21){
-                //Jovem
-                img.setAttribute('src', 'img/jovemH.jpg')
-            }else if( idade < 50){
-                //Adulto
-                img.setAttribute('src', 'img/adultoH.jpg')
-            }else{
-                //Idoso
-                img.setAttribute('src', 'img/idosoH.jpg')
-            }
-
-        }else if(fsex[1].checked){
-            genero = 'Mulher'
-
-            if(idade >=0 && idade < 10){
-                //criança
-                img.setAttribute('src', 'img/bebe.jpg')
-            }else if(idade < 21){
-                //Jovem
-                img.setAttribute('src', 'img/jovem.jpg')
-            }else if( idade < 50){
-                //Adulto
-                img.setAttribute('src', 'img/adulta.jpg')
-            }else{
-                //Idoso
-                img.setAttribute('src', 'img/idosa.jpg')
-
-            }
-        }
-        res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
-        res.appendChild(img)
-    }
+    res.style.textAlign = "center";
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos`;
+    res.appendChild(img);
+  }
 }
